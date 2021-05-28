a497d8bfdde159df6d401f01c7a7e0f4e768ed42
Neeraj <neerajbhale88@gmail.com>
2021-05-29 03:39:30 +0530

    branch

--- lib/main.dart
+++ lib/main.dart
@@ -94,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times: 1.new message 2. nothing 3. branchess',
+              'You have pushed the button this many times: 1.new message 2. nothing 3. branchess 4.merger',
             ),
             Text(
               '$_counter',
